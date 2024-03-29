09ccaef4d990bd2ed834061380b1c6206d73fde7
Dery Jon <yusupoviskandar1005@gmail.com>
2024-03-29 15:05:21 +0500

    feat: add cards orders (2024-03-29, 15:05)

--- components/kanban/useKanbanQuery.ts
+++ components/kanban/useKanbanQuery.ts
@@ -1,3 +1,41 @@
+import { useQuery } from "@tanstack/vue-query";
+import { COLLECTION_DEALS, DB_ID } from "~/app.constants";
+import type { IDeal } from "~/types/deals.types";
+import { KANBAN_DATA } from "./kanban.data";
+
 export function useKanbanQuery() {
-  return useQuer;
+  return useQuery({
+    queryKey: ["deals"],
+    queryFn: () => DB.listDocuments(DB_ID, COLLECTION_DEALS),
+    select(data) {
+      const newBoard = [] as IColumn[]; // Инициализируем пустой массив колонок
+    
+      const deals = data.documents as IDeal[]; // Получаем массив сделок из данных
+    
+      // Создаем объект, который будет содержать сделки для каждой колонки
+      const dealsByStatus: { [key: string]: IDeal[] } = {};
+    
+      // Группируем сделки по статусу
+      deals.forEach(deal => {
+        if (!dealsByStatus[deal.status]) {
+          dealsByStatus[deal.status] = [];
+        }
+        dealsByStatus[deal.status].push(deal);
+      });
+    
+      // Создаем новый объект колонки на основе KANBAN_DATA, но с добавленными сделками
+      KANBAN_DATA.forEach(columnData => {
+        const column: IColumn = {
+          id: columnData.id,
+          name: columnData.name,
+          items: dealsByStatus[columnData.id] || [], // Используем сделки для данного статуса, если они есть
+        };
+        newBoard.push(column);
+      });
+    
+      return newBoard; // Возвращаем новый объект доски
+    },
+    
+    
+  });
 }
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,44 @@
 <script setup lang="ts">
+import { useKanbanQuery } from '@/components/kanban/useKanbanQuery'
+import type { ICard, IColumn } from '~/components/kanban/kanban.types'
+import { convertCurrency } from '@/utils/convertCurrency'
+import dayjs from 'dayjs';
 useSeoMeta({
     title: 'Home | CRM System',
 })
 
 
+
 const dragCardRef = ref<ICard | null>(null)
 const sourceColumnRef = ref<IColumn | null>(null)
+
+const { data, isLoading, refetch } = useKanbanQuery()
+
 
 
 
 </script>
 <template>
     <div class="p-10">
-        <h1 class="font-bold text-2xl mb-10">CRM System bt Dery</h1>
-        <div class="">
-            <UiCard class="mb-3" draggable="true">
-                <UiCardHeader role="button">name card</UiCardHeader>
-                <UiCardContent>компания</UiCardContent>
-                <UiCardFooter>Date</UiCardFooter>
-            </UiCard>
+        <h1 class="font-bold text-2xl mb-10">CRM System by Dery</h1>
+        <div class="" v-if="isLoading">Loading...</div>
+        <div class="" v-else>
+            <div class="grid grid-cols-5 gap-6">
+                <div v-for="(column, index) in data" :key="column.id">
+                    <div class="rounded bg-slate-700 py-1 px-5 mb-2 text-center">
+                        {{ column.name }}
+                    </div>
+                    <div class="">
+
+                    </div>
+                    <UiCard v-for="card in column.items" :key="card.id" class="mb-3" draggable="true">
+                        <UiCardHeader role="button">{{ card.name }}</UiCardHeader>
+                        <UiCardContent class="opacity-70 text-xs">{{ convertCurrency(card.price) }}</UiCardContent>
+                        <UiCardContent class="text-xs"> Компания {{ card.customers.name }}</UiCardContent>
+                        <UiCardFooter>{{ dayjs(card.customers.$createdAt).format('DD MMMM YYYY') }}</UiCardFooter>
+                    </UiCard>
+                </div>
+            </div>
         </div>
     </div>
 </template>
